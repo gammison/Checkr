02deb289be55e4d9139b09b7996791b2918cd82f
Emmanuel <emmanuela956@gmail.com>
2016-08-06 15:20:30 -0500

    minor

--- client/WordToText.py
+++ client/WordToText.py
@@ -2,7 +2,7 @@ import docx
 
 doc = docx.Document('path here')
 paragraph_length = len(doc.paragraphs)
-text_file = open('test.txt', 'w')
+text_file = open('name.txt', 'w')
 for para in doc.paragraphs:
     text_file.write(para.text)
 
